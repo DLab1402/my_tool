8ced8552b062b9fc6ba5a26b1485b4e2917ccd00
DLab1402 <phankhanhduy.tg.vl@gmail.com>
2026-02-02 19:10:05 +0700

    rrrrr

--- model/gan2.py
+++ model/gan2.py
@@ -16,22 +16,6 @@ import torch
 
 os.makedirs("images", exist_ok=True)
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
-parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
-parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
-parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
-parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
-parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
-parser.add_argument("--latent_dim", type=int, default=62, help="dimensionality of the latent space")
-parser.add_argument("--img_size", type=int, default=32, help="size of each image dimension")
-parser.add_argument("--channels", type=int, default=1, help="number of image channels")
-parser.add_argument("--sample_interval", type=int, default=400, help="number of image channels")
-opt = parser.parse_args()
-print(opt)
-
-img_shape = (opt.channels, opt.img_size, opt.img_size)
-
 cuda = True if torch.cuda.is_available() else False
 
 
@@ -45,25 +29,20 @@ def weights_init_normal(m):
 
 
 class Generator(nn.Module):
-    def __init__(self):
+    vis = []
+    para = {
+            "dim":               [],
+            "Activate function": [],
+            ""
+        }
+    def __init__(self,para):
         super(Generator, self).__init__()
+        self.para = para
+        self.layer = nn.ModuleList[]
 
-        self.init_size = opt.img_size // 4
-        self.l1 = nn.Sequential(nn.Linear(opt.latent_dim, 128 * self.init_size ** 2))
-
-        self.conv_blocks = nn.Sequential(
-            nn.BatchNorm2d(128),
-            nn.Upsample(scale_factor=2),
-            nn.Conv2d(128, 128, 3, stride=1, padding=1),
-            nn.BatchNorm2d(128, 0.8),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Upsample(scale_factor=2),
-            nn.Conv2d(128, 64, 3, stride=1, padding=1),
-            nn.BatchNorm2d(64, 0.8),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(64, opt.channels, 3, stride=1, padding=1),
-            nn.Tanh(),
-        )
+    def structure_calculate(self):
+        for dim in self.para["dim"]:
+            
 
     def forward(self, noise):
         out = self.l1(noise)
@@ -90,7 +69,7 @@ class Discriminator(nn.Module):
             nn.ReLU(inplace=True),
         )
         # Upsampling
-        self.up = nn.Sequential(nn.Upsample(scale_factor=2), nn.Conv2d(64, opt.channels, 3, 1, 1))
+        self.up = nn.Sequential(nn.Upsample(scale_factor=2), nn.Conv2d(64, , 3, 1, 1))
 
     def forward(self, img):
         out = self.down(img)
