018fd1a5faec6e6ac22d10b29b08157f46a7ab34
TonyFan1402 <71585283+TonyFan1402@users.noreply.github.com>
2026-02-19 16:33:13 +0700

    rrr

--- tool/data_load/tempload.py
+++ tool/data_load/tempload.py
@@ -7,7 +7,8 @@ class TempDataset(torch.utils.data.Dataset):
     def __init__(self, root_folder):
 
         self.signals = []
-        self.labels = []  # keep if you still want labels later
+        self.labels = []
+        self.target_length = 100
 
         # list all json files in folder
         json_files = [f for f in os.listdir(root_folder) if f.endswith(".json")]
@@ -26,8 +27,15 @@ class TempDataset(torch.utils.data.Dataset):
                 # each item is a dict with keys: "Temp" and "Valid"
                 if item.get("Valid", 0) == 1:
                     temp_signal = item.get("Temp")
+                    print(len(temp_signal))
+                    if len(temp_signal) < self.target_length:
+                        padding = torch.zeros(self.target_length - len(temp_signal), dtype=temp_signal.dtype)
+                        temp_signal = torch.cat((temp_signal, padding), 0)
+                    elif len(temp_signal) > self.target_length:
+                        
+                        temp_signal = temp_signal[:self.target_length]
                     self.signals.append(temp_signal)
-                    self.labels.append(1)  # dummy label for now
+                    self.labels.append(1)
 
     def __getitem__(self, index):
         signal = torch.tensor(self.signals[index], dtype=torch.float32)
