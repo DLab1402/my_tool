8ee9ae37be896c9428d180ea7bcd3639fecde9a4
TonyFan1402 <71585283+TonyFan1402@users.noreply.github.com>
2026-02-25 12:11:42 +0700

    fix mlp

--- model/MLP.py
+++ model/MLP.py
@@ -5,26 +5,35 @@ class MLP(nn.Module):
     para = {
             "dim":               [],
             "Activate function": [],
-            "Activate last":     None
+            "Activate last":     None,
+            "BatchNorm":   False
         }
-    def __init__(self,para):
-        super(MLP, self).__init__()
+    def __init__(self, para):
+        super().__init__()
         self.para = para
         self.layers = nn.ModuleList()
-        self.structure_calculate()
+        self.build()
 
-    def structure_calculate(self):
-        for dim in self.para["dim"]:
-            self.layers.append(nn.Linear(dim[0],dim[1]))
-            if dim != self.para["dim"][-1]:
-                self.layers.append(self.para["Activate function"])
-        if self.para["Activate last"] != None:
-            self.layers.append(self.para["Activate last"])
+    def build(self):
+        dims = self.para["dim"]
+        act_fn = self.para.get("Activate function", None)
+        act_last = self.para.get("Activate last", None)
+        use_bn = self.para.get("BatchNorm", False)
+
+        for i, dim in enumerate(dims):
+            in_dim, out_dim = dim
+            self.layers.append(nn.Linear(in_dim, out_dim))
+            if use_bn:
+                self.layers.append(nn.BatchNorm1d(out_dim))
+            if i != len(dims) - 1 and act_fn is not None:
+                self.layers.append(type(act_fn)())
+
+        if act_last is not None:
+            self.layers.append(act_last)
             
-    def forward(self, noise):
+    def forward(self, x):
         self.vis.clear()
-        out = noise
         for layer in self.layers:
-            out = layer(out)
-            self.vis.append(out)
-        return out
+            x = layer(x)
+            self.vis.append(x)
+        return x
